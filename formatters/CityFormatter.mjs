9b65aefa8fa63f581a3136d9d2328f338ab57b2c
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-02-10 17:55:00 -0500

    Started on the cities

---
+++ formatters/CityFormatter.mjs
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class CityFormatter {
+
+    constructor(city) {
+        this.city = city;
+    }
+}
--- index.mjs
+++ index.mjs
@@ -4,12 +4,19 @@ import cheerio from 'cheerio';
 import DateFormatter from './formatters/DateFormatter.mjs';
 import fs from 'fs';
 
-const year1947 = fs.readFileSync('../original-sources/branham.org/1947.html');
+const year1947 = fs.readFileSync('../original-sources/branham.org/1955.html');
 const $ = cheerio.load(year1947);
 
  $('div.messagebox div.prodtext span.show-for-small-down').each((index, sermonDate) => {
-    const fullDate = $(sermonDate).text().trim();
+    const fullDate = $(sermonDate).text();
     const formatter = new DateFormatter(fullDate);
     
     console.log(formatter.format());
 });
+
+$('div.messagebox div.large-10 div.prodtext2').each((index, city) => {
+    const formattedCity = $(city).text().trim();
+    
+    console.log(formattedCity);
+});
+
