9300814760022bac6768e7a5d70f21ee626f3299
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-04-22 21:11:17 -0400

    Fixes #9 Count the Number of Months

---
+++ months.mjs
@@ -0,0 +1,28 @@
+'use strict';
+
+import fs from 'fs';
+
+const args = process.argv;
+
+if (args.length != 4) {
+    console.error('Expected two arguments: <path to database JSON file> <output file name>');
+    process.exit(1);
+}
+
+const database = args[2];
+const outputFileName = args[3];
+
+const json = JSON.parse(fs.readFileSync(database));
+const monthDictionary = {};
+
+for (let key in json) {
+    const month = json[key].date.date.substr(0, 7);
+    monthDictionary[month] = true;
+}
+
+const months = Object.keys(monthDictionary);
+
+fs.writeFileSync(outputFileName, JSON.stringify({
+    months: months,
+    total: months.length
+}, null, 4)); 
