bde782a5a667fe129c2ba613ac7f2721cd358f82
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-05-05 18:02:36 -0400

    Fixes #19 Save a Condensed Copy of the Database

--- database.mjs
+++ database.mjs
@@ -2,18 +2,20 @@
 
 import BranhamOrg from './sources/BranhamOrg.mjs';
 import Cloudinary from './sources/Cloudinary.mjs';
+import CondensedDatabasePrinter from './printers/CondensedDatabasePrinter.mjs';
 import FullDatabasePrinter from './printers/FullDatabasePrinter.mjs';
 import SpokenWordChurch from './sources/SpokenWordChurch.mjs';
 
 const args = process.argv;
 
-if (args.length != 4) {
-    console.error('Expected two arguments: <path to original sermons> <output file name>');
+if (args.length != 5) {
+    console.error('Expected two arguments: <path to original sermons> <full database output file name> <condensed database output file name>');
     process.exit(1);
 }
 
 const originalSermons = args[2];
-const outputFileName = args[3];
+const fullDatabaseFileName = args[3];
+const condensedDatabaseFileName = args[4];
 
 const masterIndex = {};
 const branhamOrg = new BranhamOrg(masterIndex, originalSermons);
@@ -25,5 +27,8 @@ spokenWordChurch.process();
 const cloudinary = new Cloudinary(masterIndex);
 cloudinary.process();
 
-const fullDatabasePrinter = new FullDatabasePrinter(masterIndex, outputFileName);
+const fullDatabasePrinter = new FullDatabasePrinter(masterIndex, fullDatabaseFileName);
 fullDatabasePrinter.print();
+
+const condensedDatabasePrinter = new CondensedDatabasePrinter(masterIndex, condensedDatabaseFileName);
+condensedDatabasePrinter.print();
