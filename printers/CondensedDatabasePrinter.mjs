3fb18722adbb47e9863a5e2d105891fd6e1cde62
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-05-05 17:57:44 -0400

    Finished condensed printer

---
+++ printers/CondensedDatabasePrinter.mjs
@@ -0,0 +1,41 @@
+'use strict';
+
+import fs from 'fs';
+
+export default class CondensedDatabasePrinter {
+
+    constructor(masterIndex, outputFileName) {
+        this.masterIndex = masterIndex;
+        this.outputFileName = outputFileName;
+    }
+
+    print() {
+        const masterArary = Object.keys(this.masterIndex).map(key => {
+            const entry = this.masterIndex[key];
+
+            return {
+                id: entry.id,
+                audio: entry.audio.secure,
+                date: {
+                    known: entry.date.known,
+                    name: entry.date.displayName
+                },
+                location: {
+                    known: entry.location.known,
+                    name: entry.location.displayName
+                },
+                title: entry.title.displayName,
+                building: {
+                    known: entry.building.known,
+                    name: entry.building.displayName
+                },
+                artwork: {
+                    large: entry.artwork.large,
+                    thumbnail: entry.artwork.thumbnail
+                }
+            };
+        });
+
+        fs.writeFileSync(this.outputFileName, JSON.stringify(masterArary, null, 4)); 
+    }
+}
